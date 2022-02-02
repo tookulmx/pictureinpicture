26f274aad74aaf5b087aba7108f36d0c2344ea4e
tookulmx <luisfragoso2.0@gmail.com>
2022-02-02 17:19:50 -0600

    New code

---
+++ script.js
@@ -0,0 +1,30 @@
+const button = document.getElementById('button-start');
+const videoElement = document.getElementById('video');
+
+// Async
+// Prompt to select media stream, pass to video Element, then play
+
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+        // Errors
+        console.log('You error is: ', error)
+    }
+}
+
+button.addEventListener('click', async () => {
+    // Deactivate button
+    button.disabled = true;
+    // Start Picture in Picture
+    await videoElement.requestPictureInPicture();
+    // Reset Button
+    button.disabled = false;
+}
+);
+// On Load
+selectMediaStream();
